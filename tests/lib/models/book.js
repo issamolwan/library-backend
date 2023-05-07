7b6b92d0500d8824d946b1b146c14802966bfbac
mr-spark <mr-spark@issamOlwan.myguest.virtualbox.org>
2023-05-07 05:23:52 +0300

    FIX: tests and model methods

--- tests/lib/models/book.js
+++ tests/lib/models/book.js
@@ -19,7 +19,7 @@ describe("Book Schema", () => {
   })
 })
 
-const newBook = {
+let newBook = {
   fbUserId: "02u2jd9j1291j",
   title: "Thinking Fast And Slow",
   total_pages: 400,
@@ -44,8 +44,9 @@ describe("Book", () => {
   })
 
   it("should insert a new book record into database", () => {
-    return mBook.post({ ...newBook }).then((result) => {
+    return mBook.post(newBook).then((result) => {
       _insert1 = result
+
       assert.isDefined(_insert1.id)
       assert.deepEqual(_insert1, {
         cover_url: [
@@ -66,18 +67,61 @@ describe("Book", () => {
     })
   })
 
-  it("should get a book record from database", () => {
-    return mBook.get(_insert1.id).then((result) => {
-      assert.deepEqual(result, _insert1)
+  it("should update a book record in database", () => {
+    newBook = {
+      fbUserId: "02u2jd9j1291j",
+      title: "Thinking Fast And Slow",
+      current_page: 320,
+    }
+    return mBook.patch(newBook).then((result) => {
+      assert.deepEqual(result, {
+        id: _insert1.id,
+        finished: false,
+        ...newBook
+      })
     })
   })
 
-  it("should update a book record in database", () => {
-    return mBook.patch(_insert1.id, { current_page: 300 }).then((result) => {
-      assert.deepEqual(result, {
-        ..._insert1,
-        current_page: 300,
+  it("should insert a new book record into database", () => {
+    newBook = {
+      fbUserId: "02u2jd9j1sihdisaw",
+      title: "Crime And Punishment",
+      total_pages: 800,
+      current_page: 242,
+    }
+    return mBook.post(newBook).then((result) => {
+      _insert1 = result
+
+      assert.isDefined(_insert1.id)
+      assert.deepEqual(_insert1, {
+        cover_url: [
+          {
+            medium: _insert1.cover_url[0].medium,
+          },
+          {
+            large: _insert1.cover_url[1].large,
+          },
+        ],
+        // TODO: look up an easier way to do this
+        fbUserId: newBook.fbUserId,
+        title: newBook.title,
+        total_pages: newBook.total_pages,
+        current_page: newBook.current_page,
+        finished: false,
+        id: _insert1.id,
       })
+    })
+  })
+
+  it("should not insert a new book record into database", () => {
+    return mBook.post({ ...newBook }).catch((err) => {
+      assert.equal(err.message, "BOOK_ALREADY_EXISTS")
+    })
+  })
+
+  it("should get all books from database for a given user", () => {
+    return mBook.get({ fbUserId: newBook.fbUserId }).then((result) => {
+      assert.equal(result.length, 2)
     })
   })
 
@@ -93,27 +137,15 @@ describe("Book", () => {
     })
   })
 
-  it("should not delete a book record from database", () => {
-    return mBook.hardDelete(_insert1.id).then((result) => {
-      assert.deepEqual(result, 0)
-    })
-  })
-
-  it("should not insert a new book record into database", () => {
-    return mBook.post({ ...newBook }).then((result) => {
-      assert.deepEqual(result, undefined)
-    })
-  })
-
-  it("should not insert a new book record into database", () => {
-    return mBook.post({ ...newBook }).then((result) => {
-      assert.deepEqual(result, undefined)
-    })
-  })
-
   it("should delete a book record from database", () => {
     return mBook.hardDelete(_insert1.id).then((result) => {
       assert.deepEqual(result, 1)
+    })
+  })
+
+  it("should not delete a book record from database", () => {
+    return mBook.hardDelete(_insert1.id).then((result) => {
+      assert.deepEqual(result, 0)
     })
   })
 
